cb4b28bf6fc69992672e4f37280d459b21b24519
Steve Taylor <steve.taylor@itaylorweb.com>
2014-06-25 11:56:09 +0100

    Revert "Changed watchFocusOut event 'focusout' to 'blur'"
    This reverts commit 4222c52a256ef07d8067d11b3e72e29438200b32.

--- src/ngAutocomplete.js
+++ src/ngAutocomplete.js
@@ -134,9 +134,9 @@ angular.module( "ngAutocomplete", [])
                           scope.details = detailsResult;
 
                           //on focusout the value reverts, need to set it again.
-                          var watchFocusOut = element.on('blur', function(event) {
+                          var watchFocusOut = element.on('focusout', function(event) {
                             element.val(detailsResult.formatted_address);
-                            element.unbind('blur')
+                            element.unbind('focusout')
                           })
 
                         });
