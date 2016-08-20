77edf68826688ec264aa64bcb53dfb46fcdd719a
Nicholai Nissen <nicholainissen@gmail.com>
2016-08-20 23:56:11 +0200

    Initial changes

--- examples/advanced.js
+++ examples/advanced.js
@@ -1,51 +1,14 @@
-angular.module( "Test", ['ngAutocomplete'])
+angular.module( "Test", ['ngAutocompleteValidate'])
   .controller("TestCtrl",function ($scope) {
-
+    $scope.validboxlabel = "Validation"
+    $scope.validbox = "invalidbox";
     $scope.result = ''
-//    $scope.details = ''
+
     $scope.options = {};
 
-    $scope.form = {
-      type: 'geocode',
-      bounds: {SWLat: 49, SWLng: -97, NELat: 50, NELng: -96},
-      country: 'ca',
-      typesEnabled: false,
-      boundsEnabled: false,
-      componentEnabled: false,
-      watchEnter: true
-    }
-
-    //watch form for changes
-    $scope.watchForm = function () {
-      return $scope.form
-    };
-    $scope.$watch($scope.watchForm, function () {
-      $scope.checkForm()
+    $scope.$watch('but.$valid', function (validity) {
+      $scope.validboxlabel = validity ? 'Valid' : 'Invalid';
     }, true);
 
 
-    //set options from form selections
-    $scope.checkForm = function() {
-
-      $scope.options = {};
-
-      $scope.options.watchEnter = $scope.form.watchEnter
-
-      if ($scope.form.typesEnabled) {
-        $scope.options.types = $scope.form.type
-      }
-      if ($scope.form.boundsEnabled) {
-
-        var SW = new google.maps.LatLng($scope.form.bounds.SWLat, $scope.form.bounds.SWLng)
-        var NE = new google.maps.LatLng($scope.form.bounds.NELat, $scope.form.bounds.NELng)
-        var bounds = new google.maps.LatLngBounds(SW, NE);
-        $scope.options.bounds = bounds
-
-      }
-      if ($scope.form.componentEnabled) {
-        $scope.options.country = $scope.form.country
-      }
-    };
-
   });
-
--- src/ngAutocomplete.js
+++ src/ngAutocomplete.js
@@ -1,166 +1,138 @@
 'use strict';
 
-/**
- * A directive for adding google places autocomplete to a text box
- * google places autocomplete info: https://developers.google.com/maps/documentation/javascript/places
- *
- * Usage:
- *
- * <input type="text"  ng-autocomplete ng-model="autocomplete" options="options" details="details/>
- *
- * + ng-model - autocomplete textbox value
- *
- * + details - more detailed autocomplete result, includes address parts, latlng, etc. (Optional)
- *
- * + options - configuration for the autocomplete (Optional)
- *
- *       + types: type,        String, values can be 'geocode', 'establishment', '(regions)', or '(cities)'
- *       + bounds: bounds,     Google maps LatLngBounds Object, biases results to bounds, but may return results outside these bounds
- *       + country: country    String, ISO 3166-1 Alpha-2 compatible country code. examples; 'ca', 'us', 'gb'
- *       + watchEnter:         Boolean, true; on Enter select top autocomplete result. false(default); enter ends autocomplete
- *
- * example:
- *
- *    options = {
- *        types: '(cities)',
- *        country: 'ca'
- *    }
-**/
+angular.module("ngAutocompleteValidate", [])
+    .directive('ngAutocompleteValidate', function() {
+        return {
+            require: 'ngModel',
+            scope: {
+                ngModel: '=',
+                options: '=?',
+                details: '=?',
+                minedAddress: '=?',
+                compiledAddress: '=?'
+            },
 
-angular.module( "ngAutocomplete", [])
-  .directive('ngAutocomplete', function() {
-    return {
-      require: 'ngModel',
-      scope: {
-        ngModel: '=',
-        options: '=?',
-        details: '=?'
-      },
+            link: function(scope, element, attrs, controller) {
 
-      link: function(scope, element, attrs, controller) {
-
-        //options for autocomplete
-        var opts
-        var watchEnter = false
-        //convert options provided to opts
-        var initOpts = function() {
-
-          opts = {}
-          if (scope.options) {
-
-            if (scope.options.watchEnter !== true) {
-              watchEnter = false
-            } else {
-              watchEnter = true
-            }
-
-            if (scope.options.types) {
-              opts.types = []
-              opts.types.push(scope.options.types)
-              scope.gPlace.setTypes(opts.types)
-            } else {
-              scope.gPlace.setTypes([])
-            }
-
-            if (scope.options.bounds) {
-              opts.bounds = scope.options.bounds
-              scope.gPlace.setBounds(opts.bounds)
-            } else {
-              scope.gPlace.setBounds(null)
-            }
-
-            if (scope.options.country) {
-              opts.componentRestrictions = {
-                country: scope.options.country
-              }
-              scope.gPlace.setComponentRestrictions(opts.componentRestrictions)
-            } else {
-              scope.gPlace.setComponentRestrictions(null)
-            }
-          }
-        }
-
-        if (scope.gPlace == undefined) {
-          scope.gPlace = new google.maps.places.Autocomplete(element[0], {});
-        }
-        google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
-          var result = scope.gPlace.getPlace();
-          if (result !== undefined) {
-            if (result.address_components !== undefined) {
-
-              scope.$apply(function() {
-
-                scope.details = result;
-
-                controller.$setViewValue(element.val());
-              });
-            }
-            else {
-              if (watchEnter) {
-                getPlace(result)
-              }
-            }
-          }
-        })
-
-        //function to get retrieve the autocompletes first result using the AutocompleteService 
-        var getPlace = function(result) {
-          var autocompleteService = new google.maps.places.AutocompleteService();
-          if (result.name.length > 0){
-            autocompleteService.getPlacePredictions(
-              {
-                input: result.name,
-                offset: result.name.length
-              },
-              function listentoresult(list, status) {
-                if(list == null || list.length == 0) {
-
-                  scope.$apply(function() {
-                    scope.details = null;
-                  });
-
-                } else {
-                  var placesService = new google.maps.places.PlacesService(element[0]);
-                  placesService.getDetails(
-                    {'reference': list[0].reference},
-                    function detailsresult(detailsResult, placesServiceStatus) {
-
-                      if (placesServiceStatus == google.maps.GeocoderStatus.OK) {
-                        scope.$apply(function() {
-
-                          controller.$setViewValue(detailsResult.formatted_address);
-                          element.val(detailsResult.formatted_address);
-
-                          scope.details = detailsResult;
-
-                          //on focusout the value reverts, need to set it again.
-                          var watchFocusOut = element.on('focusout', function(event) {
-                            element.val(detailsResult.formatted_address);
-                            element.unbind('focusout')
-                          })
-
-                        });
-                      }
-                    }
-                  );
+                function mineAddress(x) {
+                    let rAddress = {}
+                    x.address_components.forEach(function(a, i) {
+                        rAddress[a.types[0]] = a.long_name
+                    })
+                    return rAddress;
                 }
-              });
-          }
-        }
 
-        controller.$render = function () {
-          var location = controller.$viewValue;
-          element.val(location);
+                function compileAddress(x) {
+                    let rAddress = {}
+                    if (x.route) rAddress.street = x.route;
+                    if (x.street_number && rAddress.street) rAddress.street += ' ' + x.street_number;
+                    if (x.country) rAddress.country = x.country;
+                    if (x.administrative_area_level_1) {
+                        rAddress.state = x.administrative_area_level_1
+                    } else if (x.administrative_area_level_2) {
+                        rAddress.state = x.administrative_area_level_2
+                    }
+                    if (x.postal_code) rAddress.zipcode = x.postal_code
+                    if (x.locality) rAddress.city = x.locality
+                    return rAddress
+                }
+
+                if (scope.gPlace == undefined) {
+                    scope.gPlace = new google.maps.places.Autocomplete(element[0], {});
+                    scope.gPlace.setTypes(['address'])
+                }
+
+                google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                    var result = scope.gPlace.getPlace();
+                    if (result !== undefined) {
+                        if (result.address_components !== undefined) {
+                            scope.$apply(function() {
+                                scope.details = result;
+                                //console.log('result', result)
+                                controller.$setViewValue(element.val());
+                                scope.minedAddress = mineAddress(result);
+                                scope.compiledAddress = compileAddress(scope.minedAddress);
+                                controller.$validate()
+                            });
+                        } else {
+                            getPlace(result)
+                        }
+                    }
+                })
+
+                //function to get retrieve the autocompletes first result using the AutocompleteService
+                var getPlace = function(result) {
+                    var autocompleteService = new google.maps.places.AutocompleteService();
+                    if (result.name.length > 0) {
+                        autocompleteService.getPlacePredictions({
+                                input: result.name,
+                                offset: result.name.length
+                            },
+                            function listentoresult(list, status) {
+                                if (list == null || list.length == 0) {
+
+                                    scope.$apply(function() {
+                                        scope.details = null;
+                                        scope.minedAddress = null;
+                                        scope.compiledAddress = null;
+                                        controller.$validate()
+                                    });
+
+                                } else {
+                                    var placesService = new google.maps.places.PlacesService(element[0]);
+                                    placesService.getDetails({
+                                            'reference': list[0].reference
+                                        },
+                                        function detailsresult(detailsResult, placesServiceStatus) {
+
+                                            if (placesServiceStatus == google.maps.GeocoderStatus.OK) {
+                                                scope.$apply(function() {
+
+                                                    controller.$setViewValue(detailsResult.formatted_address);
+                                                    element.val(detailsResult.formatted_address);
+
+                                                    scope.details = detailsResult;
+                                                    scope.minedAddress = mineAddress(detailsResult);
+                                                    scope.compiledAddress = compileAddress(scope.minedAddress)
+                                                    controller.$validate()
+                                                    //on focusout the value reverts, need to set it again.
+                                                    var watchFocusOut = element.on('focusout', function(event) {
+                                                        element.val(detailsResult.formatted_address);
+                                                        element.unbind('focusout')
+                                                    })
+
+                                                });
+                                            }
+                                        }
+                                    );
+                                }
+                            });
+                    }
+                }
+
+
+                controller.$validators.validAddress = function() {
+                    return checkFields(scope.compiledAddress, scope.minedAddress)
+                }
+
+                function checkFields(a, b) {
+                    let valid = false
+                    console.log("TEST: ", a, b)
+                    if (!a) return false;
+                    if (a.city &&
+                        a.country &&
+                        a.street &&
+                        a.zipcode &&
+                        b.street_number) {
+                        valid = true;
+                    }
+                    return valid;
+                }
+
+                controller.$render = function() {
+                    element.val(controller.$viewValue);
+                };
+
+            }
         };
-
-        //watch options provided to directive
-        scope.watchOptions = function () {
-          return scope.options
-        };
-        scope.$watch(scope.watchOptions, function () {
-          initOpts()
-        }, true);
-
-      }
-    };
-  });
+    });
